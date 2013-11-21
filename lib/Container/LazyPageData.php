704c99b564ec9fb1fe9ca9437c79082165ea7b0b
Andreas Hennings <andreas@dqxtech.net>
2013-11-21 15:48:56 +0100

    Rich snippet / microdata support, as requested in https://drupal.org/node/2119537 by jawi.

--- lib/Container/LazyPageData.php
+++ lib/Container/LazyPageData.php
@@ -9,6 +9,7 @@
  * @property array $rawBreadcrumbItems
  * @property bool $showCurrentPage
  * @property bool $trailingSeparator
+ * @property string $richSnippetMode
  * @property bool $showFrontPage
  * @property int $minTrailItems
  * @property string $separator
